a7da502084dc0fff74c7b3482a4f1ab36503de32
Max Azarcon <contact@maxazarcon.com>
2018-04-20 12:46:27 -0400

    Push

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+/**
+ * The main index file of Project Alexandria.
+ * The purpose of this file is to load everything else.
+ */
+
+/** Loads Project Alexandria */
+require( dirname( __FILE__ ) . 'alexandria-header.php' );
